a0f9e8eb358a234ca955994ff870f57453f3f43c
Jaber Khan <md.jaberkhan66@gmail.com>
2025-01-18 15:00:03 +0600

    Variables-In-TypeScript

--- 1.Tyscript-Variables-Concepts/2.Non-Primitives-Types/1.variable-declaration.ts
+++ 1.Tyscript-Variables-Concepts/2.Non-Primitives-Types/1.variable-declaration.ts
@@ -1,3 +1,13 @@
+/*
+    Defining Array
+    ==============
+    Syntax: 
+        i)   let varName: type [] = [];                         // Native type Array
+        ii)  let varName: Array<type> = [];                     // Generic type Array
+        iii) let varName: type[][] = [ [], [], [], [], [] ];    // Multidimensional Array     
+        
+*/
+
 let heroes: string[] = ["Thor", "Captain America", "Hulk"];
 heroes.push("Iron Man"); // inserting item into the string array
 console.log(heroes);
