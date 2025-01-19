58d6c8ab775d27edd0852afa5e05082232e2f8fa
Jaber Khan <md.jaberkhan66@gmail.com>
2025-01-20 00:26:48 +0600

    TypeScripr-With-Objects

---
+++ 3.Working-With-Objects/1.objects-functionality.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function crateUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
+var userInfo = {
+    name: "JK",
+    isPaid: false,
+    email: "jk@gmail.com",
+};
+crateUser(userInfo);
---
+++ 3.Working-With-Objects/1.objects-functionality.ts
@@ -0,0 +1,10 @@
+function crateUser({ name: string, isPaid: boolean }) {}
+
+let userInfo = {
+  name: "JK",
+  isPaid: false,
+  email: "jk@gmail.com",
+};
+crateUser(userInfo);
+
+export {};
