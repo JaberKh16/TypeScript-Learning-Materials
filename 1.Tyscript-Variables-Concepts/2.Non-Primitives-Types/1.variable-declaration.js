a6f194cb5fc0ebca886c72297f1909317ef4dde7
Jaber Khan <md.jaberkhan66@gmail.com>
2025-01-24 01:47:48 +0600

    TypeScript-Variables

--- 1.Tyscript-Variables-Concepts/2.Non-Primitives-Types/1.variable-declaration.js
+++ 1.Tyscript-Variables-Concepts/2.Non-Primitives-Types/1.variable-declaration.js
@@ -1,4 +1,44 @@
 "use strict";
+/*
+    Defining Array
+    ==============
+    Syntax:
+
+    a. declaring ways:
+        i)   let varName: type [] = [];                         // Native type Array
+        ii)  let varName: Array<type> = [];                     // Generic type Array
+    b. dimension type:
+        i) let varName: type[] = [];                            // Single Dimensional Array
+        ii) let varName: type[][] = [ [], [], [], [], [] ];    // Multidimensional Array
+        
+*/
 Object.defineProperty(exports, "__esModule", { value: true });
+// defining a string type array
 var heroes = ["Thor", "Captain America", "Hulk"];
+heroes.push("Iron Man"); // inserting item into the string array
 console.log(heroes);
+// defining a number type array
+var dummyNumbers = [1, 234, 34, 32, 53, 12];
+dummyNumbers.push(50);
+console.log(dummyNumbers);
+// definw objects
+// return type: string[], number[] # strict pass
+var avengerHeroes = {
+    avengersFileHeroes: [
+        "Iron Man",
+        "Captain America",
+        "Thor",
+        "Hawkeye",
+        "Black Widow",
+        "Hulk",
+    ],
+    releaseDates: [2010, 2014, 2015, 2016, 2023, 2022, 2008],
+};
+console.log(avengerHeroes);
+// Initialize an empty array of => type: User
+var userArray = [];
+// Example: Adding users to the array
+userArray.push({ name: "Alice", designation: "Developer", salary: 80000 });
+userArray.push({ name: "Bob", designation: "Manager", salary: 95000 });
+// userArray.pop(); // pop the last item
+console.log(userArray);
--- 1.Tyscript-Variables-Concepts/2.Non-Primitives-Types/1.variable-declaration.ts
+++ 1.Tyscript-Variables-Concepts/2.Non-Primitives-Types/1.variable-declaration.ts
@@ -2,9 +2,13 @@
     Defining Array
     ==============
     Syntax: 
+
+    a. declaring ways:
         i)   let varName: type [] = [];                         // Native type Array
         ii)  let varName: Array<type> = [];                     // Generic type Array
-        iii) let varName: type[][] = [ [], [], [], [], [] ];    // Multidimensional Array     
+    b. dimension type:    
+        i) let varName: type[] = [];                            // Single Dimensional Array
+        ii) let varName: type[][] = [ [], [], [], [], [] ];    // Multidimensional Array     
         
 */
 
@@ -14,24 +18,43 @@ heroes.push("Iron Man"); // inserting item into the string array
 console.log(heroes);
 
 // defining a number type array
-let values: number[] = [1, 234, , 32, 53, 12];
-values.push(50);
-console.log(values);
+const dummyNumbers: Array<number> = [1, 234, 34, 32, 53, 12];
+dummyNumbers.push(50);
+console.log(dummyNumbers);
 
-// Custom Type Array => type Array
-// Define a User type
+// definw objects
+// return type: string[], number[] # strict pass
+const avengerHeroes: { avengersFileHeroes: string[]; releaseDates: number[] } =
+  {
+    avengersFileHeroes: [
+      "Iron Man",
+      "Captain America",
+      "Thor",
+      "Hawkeye",
+      "Black Widow",
+      "Hulk",
+    ],
+    releaseDates: [2010, 2014, 2015, 2016, 2023, 2022, 2008],
+  };
+
+console.log(avengerHeroes);
+
+// type {} example:
+// Custom/Generic Type Array => type []
+// Define a type {} : Name
 type User = {
   name: string;
   designation: string;
   salary: number;
 };
 
-// Initialize an empty array of User type
+// Initialize an empty array of => type: User
 const userArray: User[] = [];
 
 // Example: Adding users to the array
 userArray.push({ name: "Alice", designation: "Developer", salary: 80000 });
 userArray.push({ name: "Bob", designation: "Manager", salary: 95000 });
+// userArray.pop(); // pop the last item
 
 console.log(userArray);
 
