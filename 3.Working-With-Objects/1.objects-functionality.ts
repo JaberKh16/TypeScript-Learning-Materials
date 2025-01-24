cbe9a3d752fc20be2ca9a87f890fcab5834d0ad2
Jaber Khan <md.jaberkhan66@gmail.com>
2025-01-24 23:57:49 +0600

    TypeScript-With-Objects

--- 3.Working-With-Objects/1.objects-functionality.ts
+++ 3.Working-With-Objects/1.objects-functionality.ts
@@ -1,10 +1,22 @@
-function crateUser({ name: string, isPaid: boolean }) {}
+function createUser({
+  name,
+  isPaid,
+  email,
+}: {
+  name: string;
+  isPaid: boolean;
+  email?: string;
+}): { name: string; isPaid: boolean; email?: string } {
+  return { name, isPaid, email }; // Returning an object with the same structure
+}
 
 let userInfo = {
   name: "JK",
   isPaid: false,
   email: "jk@gmail.com",
 };
-crateUser(userInfo);
+
+const user = createUser(userInfo);
+console.log(user);
 
 export {};
